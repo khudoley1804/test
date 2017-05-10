45d68b23dc50867306e83a99246f380b262f91e7
MaximKhudoley <maxim180497@mail.ru>
2017-05-10 21:36:06 +0300

    Связали таблицу News c таблицей Users (связи один ко многим(обратная))

--- app/Models/News.php
+++ app/Models/News.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class News extends Model
 {
-    //
+    public function setTitleAttribute($value) {
+       $this->attributes['title'] = $value;
+       $this->attributes['slug'] = str_slug($value);
+   }
+    /**
+     * Связываем таблицу News c таблицей Users(у одного пользователя много новостей)
+     * @return type Model user
+     */
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
